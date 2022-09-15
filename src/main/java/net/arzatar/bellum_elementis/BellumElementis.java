57c5a949552d534852d967ffbb3e8faf512e83f1
Arzatar <Arzatar@gmail.com>
2022-09-14 21:44:48 -0300

    Initial commit

---
+++ src/main/java/net/arzatar/bellum_elementis/BellumElementis.java
@@ -0,0 +1,35 @@
+package net.arzatar.bellum_elementis;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BellumElementis.MOD_ID)
+public class BellumElementis {
+    public static final String MOD_ID = "bellum_elementis";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BellumElementis() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
